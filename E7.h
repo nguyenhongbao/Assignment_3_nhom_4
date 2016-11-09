2cc9252425048daf485aad7bda8926d4304b61f0
truongduy2611 <truongduy.2611@gmail.com>
2016-11-09 10:26:47 +0700

    Create E7.h
     E7) Tính trọng số của cây AVL theo độ cao – 0.5 điểm
    - Đọc file E7.txt.
    -  In cây.
    -  Tính trọng số của cây AVL theo độ cao và in chúng ra màn hình.

---
+++ E7.h
@@ -0,0 +1,48 @@
+#include "AVLTree.h"
+#include "InputReader.h"
+#include "LQueue.h"
+#include "LStack.h"
+
+void BFS2Stack(Node*pRoot, LStack<Node*>*&BFS){
+	if (pRoot != NULL){
+		Node *pTemp = pRoot;
+		Node *nullNode = new Node(0);
+		LQueue<Node*> *qTemp = new LQueue<Node*>();
+		qTemp->enQueue(pTemp);
+		while (!qTemp->isEmpty()){
+			qTemp->deQueue(pTemp);
+			Node* NewOne = new Node(pTemp->data);
+			BFS->pushStack(NewOne);
+			if (pTemp->left) qTemp->enQueue(pTemp->left);
+			if (pTemp->right) qTemp->enQueue(pTemp->right);		
+			if (pTemp->left && !pTemp->right) qTemp->enQueue(nullNode);
+			if (!pTemp->left && pTemp->right) qTemp->enQueue(nullNode);
+		}
+	}
+
+}
+
+void E7_problem(){
+	int *arr;
+	int count;
+	ReadArrayInput("input/E7.txt", arr, count);
+	AVLTree myAVL;
+	myAVL = myAVL.ArrayToAVL(arr, count);
+	myAVL.PrintAVL();
+	LStack<Node*> *sTemp = new LStack<Node*>();
+	BFS2Stack(myAVL.root, sTemp);
+	int h = 0, sum = 0;
+	sTemp = sTemp->reverse();
+	while (!sTemp->isEmpty()){
+		for (int i = 0; i < pow(2, h); i++){
+			Node* pNode = NULL;
+			if (sTemp->popStack(pNode)){
+				sum += pNode->data;
+			}
+
+		}
+		cout << sum << endl;
+		sum = 0;
+		h++;
+	}
+}
