72f9ef0155642fdf3484983c28a48560245e58f8
truongduy2611 <truongduy.2611@gmail.com>
2016-11-18 08:05:23 +0700

    Create E8.cpp

---
+++ E8.cpp
@@ -0,0 +1,24 @@
+#include "Heap.h"
+#include "InputReader.h"
+using namesapce std;
+void E8_problem(){
+	int *arr;
+	int count;
+	ReadArrayInput("input/E8.txt", arr, count);
+	Heap myHeap;
+	myHeap = myHeap.ArrayToHeap(arr, count);
+	myHeap.PrintHeapTree();
+	int h = (int)log2(count + 1) + 1; //height of tree
+	int start = 0, end = 0;
+	int sum;
+	cout << "Trong so heap theo do cao:\n";
+	for (int i = 0; i < h; i++){
+		sum = 0; //rest sum
+		end = pow(2, i) + start; // position to stop
+		for (int j = start; j < end && j < count; j++){
+			sum += myHeap[j]; 
+			start = j+1;
+		}
+		cout <<"h = " << i << ": " << sum << endl;
+	}
+};
