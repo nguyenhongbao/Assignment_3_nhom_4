dbab81e7cb5059cfea41b8a0e3862b75db9c7fe8
truongduy2611 <truongduy.2611@gmail.com>
2016-12-01 22:47:29 +0700

    fix lỗi Edge từ con sang cha

--- E10.cpp
+++ E10.cpp
@@ -3,35 +3,37 @@ void E10() {
 	int *arr = NULL;
 	int size = 0;
 
-	ReadArrayInput("input/E10.txt", arr, size);
+	if (ReadArrayInput("input/E10.txt", arr, size)){
 
-	Heap h;
-	h = h.ArrayToHeap(arr, size);
-	int **edgeDtArr;
+		Heap h;
+		h = h.ArrayToHeap(arr, size);
+		int **edgeDataArr;
 
-	edgeDtArr = new int*[size - 1];
+		edgeDataArr = new int*[size - 1];
 
-	for (int i = 0; i < size - 1; i++)
-		edgeDataArr[i] = new int[2];
+		for (int i = 0; i < size - 1; i++)
+			edgeDataArr[i] = new int[2];
 
-	for (int i = 0; i < size - 1; i++) {
-		edgeDtArr[i][0] = h[i / 2];
-		edgeDtArr[i][1] = h[i + 1];
-		edgeDtArr[0][i] = h[i / 2];
-		edgeDtArr[1][i] = h[i + 1];
+		for (int i = 0; i < size - 1; i++) {
+			edgeDataArr[i][0] = h[i / 2];
+			edgeDataArr[i][1] = h[i + 1];
+			edgeDataArr[0][i] = h[i / 2];
+			edgeDataArr[1][i] = h[i + 1];
+		}
+
+		Graph g;
+
+		for (int i = 0; i < size; i++) {
+			g.InsertVertex(h[i]);
+		}
+
+		for (int i = 0; i < size - 1; i++) {
+			g.InsertEdge(edgeDataArr[i][0], edgeDataArr[i][1]);
+			g.InsertEdge(edgeDataArr[i][1], edgeDataArr[i][0]);
+		}
+
+		g.Print();
+
+		delete edgeDataArr;
 	}
-
-	Graph g;
-
-	for (int i = 0; i < size; i++) {
-		g.InsertVertex(h[i]);
-	}
-
-	for (int i = 0; i < size - 1; i++) {
-		g.InsertEdge(edgeDtArr[i][0], edgeDtArr[i][1]);
-	}
-
-	g.Print();
-
-	delete edgeDtArr;
 }
