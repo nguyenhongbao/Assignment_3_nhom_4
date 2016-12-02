cbcb9ab452babf1b195a18db5658ff05e84836f3
truongduy2611 <truongduy.2611@gmail.com>
2016-12-02 16:58:22 +0700

    Update E7.cpp

--- E7.cpp
+++ E7.cpp
@@ -11,21 +11,26 @@ void BFS2Stack(Node*pRoot, LStack<Node*>*&BFS){
 		qTemp->enQueue(pTemp);
 		while (!qTemp->isEmpty()){
 			qTemp->deQueue(pTemp);
-			Node* NewOne = new Node(pTemp->data);
+			Node* NewOne = new Node(pTemp->data);		
 			BFS->pushStack(NewOne);
-			if (pTemp->left) qTemp->enQueue(pTemp->left);
-			if (pTemp->right) qTemp->enQueue(pTemp->right);		
-			if (pTemp->left && !pTemp->right) qTemp->enQueue(nullNode);
-			if (!pTemp->left && pTemp->right) qTemp->enQueue(nullNode);
+			if (pTemp->data != 0){				
+				if (pTemp->left) qTemp->enQueue(pTemp->left);
+				if (pTemp->right) qTemp->enQueue(pTemp->right);
+				if (pTemp->left && !pTemp->right) qTemp->enQueue(nullNode);
+				if (!pTemp->left && pTemp->right) qTemp->enQueue(nullNode);
+				if (!pTemp->left && !pTemp->right){
+					qTemp->enQueue(nullNode);
+					qTemp->enQueue(nullNode);
+				}
+			}
 		}
 	}
 
 }
-
 void E7_problem(){
 	int *arr;
 	int count;
-	if(ReadArrayInput("input/E7.txt", arr, count)){
+	if (ReadArrayInput("input/E7.txt", arr, count)){
 		AVLTree myAVL;
 		myAVL = myAVL.ArrayToAVL(arr, count);
 		myAVL.PrintAVL();
@@ -34,6 +39,7 @@ void E7_problem(){
 		int h = 0, sum = 0;
 		sTemp = sTemp->reverse();
 		while (!sTemp->isEmpty()){
+			sum = 0; //rest sum
 			for (int i = 0; i < pow(2, h); i++){
 				Node* pNode = NULL;
 				if (sTemp->popStack(pNode)){
@@ -41,8 +47,7 @@ void E7_problem(){
 				}
 
 			}
-			cout << sum << endl;
-			sum = 0;
+			if (sum > 0) cout << sum << endl;		
 			h++;
 		}
 	}
