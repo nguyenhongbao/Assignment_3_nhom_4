f4d01a45327d1cb0f03c889091c722adcd3a4c5a
truongduy2611 <truongduy.2611@gmail.com>
2016-12-04 22:35:44 +0700

    sua lai

--- E6.cpp
+++ E6.cpp
@@ -1,32 +1,32 @@
-#include "Graph.h"
-using namespace std;
-
-
 bool Graph::RemoveEdge(Vertex* from, Edge* from_to){
 	if (!from || !from_to) return false;
 	if (!this->VertexExist(from)) return false;
 	Edge* eG = from->firstEdge, *ePre = from->firstEdge;
 	if (from->firstEdge == from_to){
 		from->firstEdge = from->firstEdge->nextEdge;
+		eG->destination->inDegree--;
 		delete eG;
 		eG = NULL;
+		from->outDegree--;
 		return true;
 	}
 	while (eG){
 		if (eG == from_to)
 		{
-			if (eG->nextEdge) { 
+			if (eG->nextEdge) {
 				ePre->nextEdge = eG->nextEdge;
 			}
-			else { 
-				ePre->nextEdge = eG = NULL; 
+			else {
+				ePre->nextEdge = eG = NULL;
 			}
+			from_to->destination->inDegree--;
 			delete from_to;
 			from_to = NULL;
+			from->outDegree--;
 			return true;
 		}
 		ePre = eG;
-		if (eG->nextEdge) eG = eG->nextEdge; 
+		eG = eG->nextEdge;
 	}
 	return false;
 
@@ -36,7 +36,7 @@ bool  Graph::RemoveEdge(Vertex* from, Vertex* to){
 	if (!this->VertexExist(from) || !this->VertexExist(to)) return false;
 	Edge* eG = from->firstEdge;
 	while (eG){
-		if (eG->destination == to) { 
+		if (eG->destination == to) {
 			return this->RemoveEdge(from, eG);
 		}
 		eG = eG->nextEdge;
@@ -45,122 +45,76 @@ bool  Graph::RemoveEdge(Vertex* from, Vertex* to){
 }
 bool Graph::RemoveEdge(int fromData, int toData){
 	//if (this->VertexExist(fromData) && this->VertexExist(toData))
-		return RemoveEdge(GetVertex(fromData), GetVertex(toData));
+	return RemoveEdge(GetVertex(fromData), GetVertex(toData));
 	//return false;
 }
 
-bool Graph::RemoveVertex(Vertex* del) 
+bool Graph::RemoveVertex(Vertex* del)
 {
 	if (del == NULL) return false;
-	Vertex* Pre = NULL;
-	Vertex* Cur = this->gHead;
-	Edge* Ed = this->gHead->firstEdge;	
-	while (Cur)
+	//xoa Edge tu Vertex can xoa
+	Vertex* vTemp = gHead;
+	Edge* eG = NULL;
+	while (vTemp)
 	{
-		if (del == this->gHead) 
-		{
-			// Xoa vertex hien tai		
-			Edge* temp;
-			while (this->gHead->firstEdge)
-			{				
-				temp = this->gHead->firstEdge;
-				this->gHead->firstEdge = this->gHead->firstEdge->nextEdge;
-				delete temp; temp = NULL;
-				Ed = Ed->nextEdge;
+		eG = vTemp->firstEdge;
+		while (eG && vTemp->firstEdge){
+			if (eG->destination == del){ 
+				RemoveEdge(vTemp, del); 
 			}
-			// xoa dinh
-			this->gHead = Cur->nextVertex;
-			delete Cur; Cur = NULL;
+			eG = eG->nextEdge;
+		}
+		vTemp = vTemp->nextVertex;
+	}
+	vTemp = gHead;
+	Vertex *vPre = gHead;
+	if (gHead == del){
+		gHead = gHead->nextVertex;
+		delete del;
+		del = NULL;
+		this->size--;
+		return true;
+	}
+	while (vTemp != NULL){
+		if (vTemp->data == del->data)
+		{
+			if (vTemp->nextVertex) {
+				vPre->nextVertex = vTemp->nextVertex;
+			}
+			else {
+				vPre->nextVertex =  NULL;
+			}
+			this->size--;
+			delete vTemp;
+			vTemp = del = NULL;
 			return true;
 		}
-		else if (Cur == del) // xac dinh vertex can xoa
-		{
-			// xoa danh sach cac canh cua dinh hien tai
-			Edge* temp;
-			while (this->gHead->firstEdge)
-			{
-				temp = this->gHead->firstEdge;
-				this->gHead->firstEdge = this->gHead->firstEdge->nextEdge;
-				delete temp; temp = NULL;
-				Ed = Ed->nextEdge;
-			}
-			// xoa dinh
-			Pre->nextVertex = Cur->nextVertex;
-			delete Cur; Cur = NULL;
-			return true;
-		}
-		// xoa cac canh noi tu cac dinh khac noi toi dinh can xoa
-		Pre = Cur;
-		Cur = Cur->nextVertex;
+		vPre = vTemp;
+		vTemp = vTemp->nextVertex;
 	}
 	return false;
 }
 
-bool Graph::RemoveVertex(int reData) 
+bool Graph::RemoveVertex(int reData)
 {
-	Vertex* del = this->GetVertex(reData);
-	if (del == NULL) return false;
-	Vertex* Pre = NULL;
-	Vertex* Cur = this->gHead;
-	Edge* Ed = this->gHead->firstEdge;	
-	Edge* cur = Cur->firstEdge;
-	Edge* pre = NULL;
-	Edge* temp = NULL;	
-	while (Cur)
-	{
-		if (del == this->gHead)
-		{
-			// xoa danh sach cac canh cua dinh hien tai						
-			while (this->gHead->firstEdge)
-			{
-				temp = this->gHead->firstEdge;
-				this->gHead->firstEdge = this->gHead->firstEdge->nextEdge;
-				delete temp; temp = NULL;
-				Ed = Ed->nextEdge;
-			}
-			// xoa dinh
-			this->gHead = Cur->nextVertex;
-			delete Cur; Cur = NULL;
-			return true;
-		}
-		else if (Cur == del) // da tim duoc vertex can xoa
-		{
-			// xoa danh sach cac canh cua dinh hien tai			
-			while (this->gHead->firstEdge)
-			{
-				temp = this->gHead->firstEdge;
-				this->gHead->firstEdge = this->gHead->firstEdge->nextEdge;
-				delete temp; temp = NULL;
-				Ed = Ed->nextEdge;
-			}
-			// xoa dinh
-			Pre->nextVertex = Cur->nextVertex;
-			delete Cur; Cur = NULL;
-			return true;
-		}		
-		// xoa cac canh noi tu cac dinh khac noi toi dinh can xoa	
-		cur = Cur->firstEdge;
-		while (cur) 
-		{
-			if (reData == Cur->firstEdge->destination->data)
-			{
-				temp = Cur->firstEdge;
-				Cur->firstEdge = temp->nextEdge;
-				delete temp; temp = NULL;		
-				break;
-			}
-			else if (reData == cur->destination->data) // da tim thay canh can xoa
-			{
-				pre->nextEdge = cur->nextEdge;
-				delete cur; cur = NULL;			
-				break;
-			}
-			pre = cur;
-			cur = cur->nextEdge;
-		}
-		Pre = Cur;
-		Cur = Cur->nextVertex;
-	}
-	return false;
+	if (!VertexExist(reData)) return false;
+	return RemoveVertex(GetVertex(reData));
 
+}
+void E6() {
+	Graph graph;
+	MakeGraph(graph, "input/E6.txt");
+	graph.Print();
+	// vong lap xoa
+	while (true){
+		int delVertex;
+		cout << "Moi ban nhap chi so cua vertex can xoa (neu khong muon xoa nua nhap -1) : ";
+		cin >> delVertex;
+		if (delVertex == -1) break;
+		if (!graph.VertexExist(delVertex)) cout << "Chi so dinh ban nhap khong co trong graph! Vui long nhap lai!\n" << endl;
+		else {
+			graph.RemoveVertex(delVertex);
+			graph.Print();
+		}
+	}
 }
